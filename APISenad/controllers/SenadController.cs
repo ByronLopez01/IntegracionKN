2dd2047cf992bdfe9e89fb764ce2eb0ef5553ac0
Cristian Anjari <khrizapellido@gmail.com>
2024-12-11 17:26:45 -0300

    Update GET test/{codItem} SenadController

--- APISenad/controllers/SenadController.cs
+++ APISenad/controllers/SenadController.cs
@@ -16,94 +16,6 @@ namespace APISenad.controllers
         {
             _context = context;
         }
-        /*
-        [HttpGet("{codItem}")]
-        public async Task<ActionResult> consultaCodigo(string codItem)
-        {
-            // Verifica que el código de ítem no esté vacío
-            if (string.IsNullOrEmpty(codItem))
-            {
-                return BadRequest("El código del ítem no puede estar vacío.");
-            }
-
-            // Busca el código en los campos codMastr, codInr y codProducto en la tabla ordenesEnProceso
-            var ordenesEncontradas = await _context.ordenesEnProceso
-                .Where(o => o.codMastr == codItem || o.codInr == codItem || o.codProducto == codItem)
-                .ToListAsync();
-
-            Console.WriteLine($"Buscando código: {codItem}");
-            Console.WriteLine($"Registros encontrados: {ordenesEncontradas.Count}");
-
-            if (ordenesEncontradas == null || !ordenesEncontradas.Any())
-            {
-                return NotFound($"No se encontró ningún registro con el código {codItem}.");
-            }
-
-            int salida = 0;
-            string ordennum = "";
-
-            foreach (var orden in ordenesEncontradas)
-            {
-                string tipoCodigo = "Desconocido";
-                int cantidadProcesada = 0;
-
-                if (orden.codMastr == codItem)
-                {
-                    tipoCodigo = "Master";
-                    // Sumar la cantidad master a cantidad procesada
-                    cantidadProcesada = orden.cantMastr + orden.cantidadProcesada;
-                }
-                else if (orden.codInr == codItem)
-                {
-                    tipoCodigo = "Inner";
-                    // Sumar la cantidad del campo cantidadInr
-                    cantidadProcesada = orden.cantInr + orden.cantidadProcesada;
-                }
-                else if (orden.codProducto == codItem)
-                {
-                    //No deberia pasar!!!!!
-                    tipoCodigo = "Producto";
-                    // Sumar la cantidad del campo cantidad
-                    cantidadProcesada = orden.cantidad + orden.cantidadProcesada;
-                }
-
-                salida = orden.numSalida;
-                ordennum = orden.numOrden;
-
-                // Mensaje de depuración
-                Console.WriteLine($"Registro encontrado: ID={orden.numOrden}, codMastr={orden.codMastr}, codInr={orden.codInr}, codProducto={orden.codProducto}, Tipo={tipoCodigo}");
-                Console.WriteLine($"Cantidad procesada actualizada: {cantidadProcesada}");
-
-                // Verifica si la cantidad procesada supera la cantidad total permitida
-                if (cantidadProcesada > orden.cantidad)
-                {
-                    var response = new
-                    {
-                        CodigoEscaneado = codItem,
-                        NumeroOrden = ordennum,
-                        Salida = 0,//Asignar salida de error 
-                        Error = "La cantidad a procesar supera la cantidad permitida."
-                    };
-                    return BadRequest(response);
-                }
-
-                // Actualiza la cantidad procesada solo si no supera la cantidad total
-                orden.cantidadProcesada = cantidadProcesada;
-                _context.ordenesEnProceso.Update(orden);
-            }
-
-            await _context.SaveChangesAsync();
-
-            var respuestaSorter = new RespuestaEscaneo
-            {
-                codigoIngresado = codItem,
-                numeroOrden = ordennum,
-                salida = salida
-            };
-
-            return Ok(respuestaSorter);
-        }
-        */
 
         [HttpGet("{codItem}")]
         public async Task<ActionResult> consultaCodigo(string codItem)
@@ -232,58 +144,30 @@ namespace APISenad.controllers
             return Ok(respuestaSorter);
         }
 
+        // TEST!!
         [HttpGet("test/{codItem}")]
         public async Task<ActionResult> codigoEscaneado(string codItem)
         {
+            // Verifica que el código de ítem no esté vacío
             if (string.IsNullOrEmpty(codItem))
             {
                 return BadRequest("El código del ítem no puede estar vacío.");
             }
 
-            // Buscar si el código pertenece a alguna orden en proceso
+            // Busca el código en los campos codMastr, codInr y codProducto en la tabla ordenesEnProceso
             var ordenesEncontradas = await _context.ordenesEnProceso
                 .Where(o => o.codMastr == codItem || o.codInr == codItem || o.codProducto == codItem)
                 .ToListAsync();
 
-            if (!ordenesEncontradas.Any())
-            {
-                // Verificar si el código pertenece a una familia con tanda activa en FamilyMaster
-                var familiaActiva = await _context.Familias
-                    .Where(f => (f.Tienda1 == codItem || f.Tienda2 == codItem || f.Tienda3 == codItem ||
-                                 f.Tienda4 == codItem || f.Tienda5 == codItem || f.Tienda6 == codItem ||
-                                 f.Tienda7 == codItem || f.Tienda8 == codItem || f.Tienda9 == codItem ||
-                                 f.Tienda10 == codItem || f.Tienda11 == codItem || f.Tienda12 == codItem) // ||
-                                 //f.tienda13 == codItem || f.tienda14 == codItem)
-                                 && f.estado == true) // Verificar si la tanda está activa
-                    .FirstOrDefaultAsync();
+            Console.WriteLine($"Buscando código: {codItem}");
+            Console.WriteLine($"Registros encontrados: {ordenesEncontradas.Count}");
 
-                if (familiaActiva != null)
-                {
-                    // Si pertenece a una familia activa, devolver información de la familia y tanda
-                    var response = new
-                    {
-                        CodigoEscaneado = codItem,
-                        Familia = familiaActiva.Familia,
-                        NumeroTanda = familiaActiva.NumTanda,
-                        Salida = familiaActiva.NumSalida,
-                        Estado = "Tanda Activa"
-                    };
-                    return Ok(response);
-                }
-                else
-                {
-                    // Si no pertenece a una familia activa
-                    var responseError = new
-                    {
-                        CodigoEscaneado = codItem,
-                        Error = "El código no pertenece a una familia con tanda activa."
-                    };
-                    return NotFound(responseError);
-                }
+            if (ordenesEncontradas == null || !ordenesEncontradas.Any())
+            {
+                return NotFound($"No se encontró ningún registro con el código {codItem}.");
             }
 
-            // Lógica existente para las órdenes encontradas
-            int salidaAsignada = ordenesEncontradas.First().numSalida;
+            int salida = 0;
             string ordennum = "";
 
             foreach (var orden in ordenesEncontradas)
@@ -294,49 +178,58 @@ namespace APISenad.controllers
                 if (orden.codMastr == codItem)
                 {
                     tipoCodigo = "Master";
+                    // Sumar la cantidad master a cantidad procesada
                     cantidadProcesada = orden.cantMastr + orden.cantidadProcesada;
                 }
                 else if (orden.codInr == codItem)
                 {
                     tipoCodigo = "Inner";
+                    // Sumar la cantidad del campo cantidadInr
                     cantidadProcesada = orden.cantInr + orden.cantidadProcesada;
                 }
                 else if (orden.codProducto == codItem)
                 {
+                    //No deberia pasar!!!!!
                     tipoCodigo = "Producto";
+                    // Sumar la cantidad del campo cantidad
                     cantidadProcesada = orden.cantidad + orden.cantidadProcesada;
                 }
 
+                salida = orden.numSalida;
                 ordennum = orden.numOrden;
 
+                // Mensaje de depuración
+                Console.WriteLine($"Registro encontrado: ID={orden.numOrden}, codMastr={orden.codMastr}, codInr={orden.codInr}, codProducto={orden.codProducto}, Tipo={tipoCodigo}");
+                Console.WriteLine($"Cantidad procesada actualizada: {cantidadProcesada}");
+
+                // Verifica si la cantidad procesada supera la cantidad total permitida
                 if (cantidadProcesada > orden.cantidad)
                 {
                     var response = new
                     {
                         CodigoEscaneado = codItem,
                         NumeroOrden = ordennum,
-                        Salida = 9,
+                        Salida = 0,//Asignar salida de error 
                         Error = "La cantidad a procesar supera la cantidad permitida."
                     };
                     return BadRequest(response);
                 }
 
+                // Actualiza la cantidad procesada solo si no supera la cantidad total
                 orden.cantidadProcesada = cantidadProcesada;
                 _context.ordenesEnProceso.Update(orden);
             }
 
             await _context.SaveChangesAsync();
 
-            var respuestaSorter = new
+            var respuestaSorter = new RespuestaEscaneo
             {
                 codigoIngresado = codItem,
                 numeroOrden = ordennum,
-                salida = salidaAsignada
+                salida = salida
             };
 
             return Ok(respuestaSorter);
         }
-
-
     }
 }
