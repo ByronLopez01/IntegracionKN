c73c8a0c8b765d2f3985309efac34fb906edd824
SnakeJuice <93675228+SnakeJuice@users.noreply.github.com>
2024-12-26 13:34:37 -0300

    OrderCancel avance

--- APIOrderUpdate/services/OrderCancelService.cs
+++ APIOrderUpdate/services/OrderCancelService.cs
@@ -18,59 +18,64 @@ namespace APIOrderUpdate.services
 
         public async Task<OrderCancelResult> HandleOrderCancelAsync(OrderCancelKN orderCancelKn)
         {
-            var waveId = orderCancelKn.ORDER_CANCEL.ORDER_CANCEL_SEG.schbat;
-            var ordnum = orderCancelKn.ORDER_CANCEL.ORDER_CANCEL_SEG.ordnum;
-
-            var existingWaveRelease = await _context.WaveReleases
-                .Where(w => w.Wave == waveId && w.NumOrden == ordnum)
-                .FirstOrDefaultAsync();
-
-            if (existingWaveRelease != null)
-            { 
-                _context.WaveReleases.Remove(existingWaveRelease);
-
-               var newOrderCancel = new OrderCancelEntity
-                {
-                    Wave = waveId,
-                    WhId = orderCancelKn.ORDER_CANCEL.wh_id,
-                    MsgId = orderCancelKn.ORDER_CANCEL.msg_id,
-                    Trandt = orderCancelKn.ORDER_CANCEL.trandt,
-                    Ordnum = ordnum,
-                    Schbat = orderCancelKn.ORDER_CANCEL.ORDER_CANCEL_SEG.schbat,
-                    Cancod = orderCancelKn.ORDER_CANCEL.ORDER_CANCEL_SEG.cancod,
-                    Accion = "Cancelación"
-                };
-
-                _context.ordenes.Add(newOrderCancel);
-                await _context.SaveChangesAsync();
-
-                return OrderCancelResult.Cancelled;
-            }
-            else
+            if (orderCancelKn.ORDER_CANCEL.ORDER_CANCEL_SEG is not IEnumerable<OrderCancelSeg> orderCancelSegs)
             {
-                // Buscar si la orden existe en la base de datos en otras Waves
-                var anyOrderExists = await _context.WaveReleases
-                    .Where(w => w.NumOrden == ordnum)
-                    .AnyAsync();
-
-                var newOrderCancel = new OrderCancelEntity
-                {
-                    Wave = waveId,
-                    WhId = orderCancelKn.ORDER_CANCEL.wh_id,
-                    MsgId = orderCancelKn.ORDER_CANCEL.msg_id,
-                    Trandt = orderCancelKn.ORDER_CANCEL.trandt,
-                    Ordnum = ordnum,
-                    Schbat = orderCancelKn.ORDER_CANCEL.ORDER_CANCEL_SEG.schbat,
-                    Cancod = orderCancelKn.ORDER_CANCEL.ORDER_CANCEL_SEG.cancod,
-                    Accion = anyOrderExists ? "Orden no encontrada en la Wave" : "Orden no encontrada"
-                };
-
-                _context.ordenes.Add(newOrderCancel);
-                await _context.SaveChangesAsync();
-
-                return anyOrderExists ? OrderCancelResult.NotFound : OrderCancelResult.NotFound;
+                return OrderCancelResult.NotFound; //Ordenes no encontradas
             }
-        }
 
+            foreach (var orderCancelSeg in orderCancelSegs)
+            {
+                var waveId = orderCancelSeg.schbat;
+                var ordnum = orderCancelSeg.ordnum;
+
+                var existingWaveRelease = await _context.WaveReleases
+                    .Where(w => w.Wave == waveId && w.NumOrden == ordnum)
+                    .FirstOrDefaultAsync();
+
+                if (existingWaveRelease != null)
+                {
+                    _context.WaveReleases.Remove(existingWaveRelease);
+
+                    var newOrderCancel = new OrderCancelEntity
+                    {
+                        Wave = waveId,
+                        WhId = orderCancelKn.ORDER_CANCEL.wh_id,
+                        MsgId = orderCancelKn.ORDER_CANCEL.msg_id,
+                        Trandt = orderCancelKn.ORDER_CANCEL.trandt,
+                        Ordnum = ordnum,
+                        Schbat = orderCancelSeg.schbat,
+                        Cancod = orderCancelSeg.cancod,
+                        Accion = "Cancelación"
+                    };
+
+                    _context.ordenes.Add(newOrderCancel);
+                }
+                else
+                {
+                    // Buscar si la orden existe en la base de datos en otras Waves
+                    var anyOrderExists = await _context.WaveReleases
+                        .Where(w => w.NumOrden == ordnum)
+                        .AnyAsync();
+
+                    var newOrderCancel = new OrderCancelEntity
+                    {
+                        Wave = waveId,
+                        WhId = orderCancelKn.ORDER_CANCEL.wh_id,
+                        MsgId = orderCancelKn.ORDER_CANCEL.msg_id,
+                        Trandt = orderCancelKn.ORDER_CANCEL.trandt,
+                        Ordnum = ordnum,
+                        Schbat = orderCancelSeg.schbat,
+                        Cancod = orderCancelSeg.cancod,
+                        Accion = anyOrderExists ? "Orden no encontrada en la Wave" : "Orden no encontrada"
+                    };
+
+                    _context.ordenes.Add(newOrderCancel);
+                }
+            }
+
+            await _context.SaveChangesAsync();
+
+            return OrderCancelResult.Cancelled; //Ordenes canceladas
+        }
     }
 }
