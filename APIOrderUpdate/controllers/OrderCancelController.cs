e29db5e408ad3f54349173d3becb9487641539c5
Cristian Anjari <93675228+SnakeJuice@users.noreply.github.com>
2025-01-26 21:25:21 -0300

    Agregado envío a Luca en OrderCancel

--- APIOrderUpdate/controllers/OrderCancelController.cs
+++ APIOrderUpdate/controllers/OrderCancelController.cs
@@ -1,9 +1,14 @@
-﻿using APIOrderUpdate.data;
+﻿using System.Text;
+using APIOrderUpdate.data;
 using APIOrderUpdate.enums;
 using APIOrderUpdate.models;
 using APIOrderUpdate.services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace APIOrderUpdate.controllers
 {
@@ -13,10 +18,16 @@ namespace APIOrderUpdate.controllers
     public class OrderCancelController : ControllerBase
     {
         private readonly IOrderCancelService _orderCancelService;
+        private readonly HttpClient _httpClient;
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConfiguration _configuration;
 
-        public OrderCancelController(IOrderCancelService orderCancelService)
+        public OrderCancelController(IOrderCancelService orderCancelService, IHttpClientFactory httpClientFactory, IConfiguration configuration)
         {
             _orderCancelService = orderCancelService;
+            _httpClientFactory = httpClientFactory;
+            _httpClient = _httpClientFactory.CreateClient();
+            _configuration = configuration;
         }
 
         [HttpPost]
@@ -31,6 +42,45 @@ namespace APIOrderUpdate.controllers
 
             var (result, ordersNotCancelled) = await _orderCancelService.HandleOrderCancelAsync(orderCancelKn);
 
+            var ordersToCancel = orderCancelKn.ORDER_CANCEL.ORDER_CANCEL_SEG
+                .Where(seg => !ordersNotCancelled.Contains(seg.ordnum))
+                .ToList();
+
+            if (ordersToCancel.Any())
+            {
+                var orderCancelJson = new
+                {
+                    ORDER_CANCEL = new
+                    {
+                        orderCancelKn.ORDER_CANCEL.wcs_id,
+                        orderCancelKn.ORDER_CANCEL.wh_id,
+                        orderCancelKn.ORDER_CANCEL.msg_id,
+                        orderCancelKn.ORDER_CANCEL.trandt,
+                        ORDER_CANCEL_SEG = ordersToCancel
+                    }
+                };
+
+                // VERIFICAR SI LA URL APUNTA A PROD O QA!!!
+                var jsonContent = new StringContent(JsonSerializer.Serialize(orderCancelJson), Encoding.UTF8, "application/json");
+                var urlLuca = _configuration["ServiceUrls:luca"];
+
+                Console.WriteLine($"URL Luca: {urlLuca}/api/sort/OrderUpdate");
+                Console.WriteLine($"URL Luca: {urlLuca}/api/sort/OrderUpdate");
+                Console.WriteLine($"URL Luca: {urlLuca}/api/sort/OrderUpdate");
+
+                // Console.WriteLine de el contenido del json
+                Console.WriteLine(JsonSerializer.Serialize(orderCancelJson));
+
+                // ENVIO A LUCA!!
+                var response = await _httpClient.PostAsync($"{urlLuca}/api/sort/OrderUpdate", jsonContent);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return StatusCode((int)response.StatusCode, "Ocurrió un error al enviar el OrderCancel a Luca.");
+                }
+                
+            }
+
             switch (result)
             {
                 case OrderCancelResult.AllOrdersInProcess:
@@ -42,15 +92,17 @@ namespace APIOrderUpdate.controllers
                 case OrderCancelResult.PartiallyCancelled:
                     return BadRequest(new
                     {
-                        message = "Las órdenes fueron canceladas. Exceptuando las que faltan por procesar.",
+                        message = "Las órdenes fueron canceladas. Exceptuando las que se encuentran procesadas.",
                         ordersNotCancelled
                     });
                 case OrderCancelResult.NotFound:
                     return NotFound("La orden no fue encontrada y se ha registrado con un mensaje.");
+
                 case OrderCancelResult.Cancelled:
                     return Ok("La orden ha sido cancelada exitosamente.");
+
                 default:
-                    return StatusCode(500, "Ocurrió un error inesperado.");
+                    return StatusCode((int)Response.StatusCode, "Ocurrió un error al cancelar las ordenes.");
             }
         }
     }
